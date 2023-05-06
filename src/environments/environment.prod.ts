c564cac957278c081c9df4bdf91ebd1dcc75bb30
Zayar Linn Naung <zayarlinnnaung.coder@gmail.com>
2023-05-06 13:09:34 +0630

    update: update cv form

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   spaceId: 'hvjg5hdex80w',
-  accessToken: 'ZMh-8-xpliQsoeNHkzLv0ZAw2gp0o1f0GxBqINTTK3E'
+  accessToken: 'ZMh-8-xpliQsoeNHkzLv0ZAw2gp0o1f0GxBqINTTK3E',
+  devz: 'blog'
 };
